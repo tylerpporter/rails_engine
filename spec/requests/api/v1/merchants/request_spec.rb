7392c6a99552b8eca5b6be4e80f4da6e2ed636b5
Tyler Porter <tylerpporter@gmail.com>
2020-05-18 12:13:33 -0600

    Successful get request/response for single merchant

--- spec/requests/api/v1/merchants/request_spec.rb
+++ spec/requests/api/v1/merchants/request_spec.rb
@@ -9,5 +9,17 @@ describe 'API - Merchants resource' do
 
     expect(merchants[:data].size).to eq(5)
   end
+  it 'sends one merchant by id' do
+    create_list(:merchant, 5)
+    merchant = Merchant.last
+
+    get "/api/v1/merchants/#{merchant.id}"
+
+    resp = JSON.parse(response.body, symbolize_names: true)
+
+    expect(resp[:data][:attributes][:id]).to eq(merchant.id)
+    expect(resp[:data][:type]).to eq("merchant")
+    expect(resp[:data][:attributes][:name]).to eq(merchant.name)
+  end
 
 end
