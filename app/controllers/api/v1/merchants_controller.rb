2995043847a922a51947639fb1a5c3a0cecd3eea
Tyler Porter <tylerpporter@gmail.com>
2020-05-18 12:27:44 -0600

    Successful patch request/response for merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -11,10 +11,14 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(Merchant.create(merchant_params))
   end
 
+  def update
+    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
+  end 
+
   private
 
   def merchant_params
     params.permit(:name)
   end
-  
+
 end
--- spec/requests/api/v1/merchants/request_spec.rb
+++ spec/requests/api/v1/merchants/request_spec.rb
@@ -22,16 +22,33 @@ describe 'API - Merchants resource' do
     expect(resp[:data][:attributes][:name]).to eq(merchant.name)
   end
   it 'can create a merchant' do
-    body = {
-      "name": "Bobby's Shop Of Hops"
-    }
+    body = {"name": "Bobby's Shop Of Hops"}
 
     post '/api/v1/merchants', params: body
 
     resp = JSON.parse(response.body, symbolize_names: true)
 
+    expect(resp[:data][:type]).to eq("merchant")
     expect(resp[:data][:attributes][:id]).to be_present
     expect(resp[:data][:attributes][:name]).to eq(body[:name])
+  end
+  it 'can update a merchant' do
+    create_list(:merchant, 5)
+    merchant = Merchant.last
+    body = {"name": "Bobby's Shop Of Hops"}
+
+    expect(merchant.name).to_not eq(body[:name])
+
+    patch "/api/v1/merchants/#{merchant.id}", params: body
+
+    resp = JSON.parse(response.body, symbolize_names: true)
+
+    expect(resp[:data][:attributes][:name]).to eq(body[:name])
+    expect(resp[:data][:attributes][:id]).to eq(merchant.id)
+
+    merchant.reload
+
+    expect(resp[:data][:attributes][:name]).to eq(merchant.name)
   end
 
 end
