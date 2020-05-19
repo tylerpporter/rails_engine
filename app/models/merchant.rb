e1cfa3cb9b049a4a573e7ce98c419ff9a2ab0238
Tyler Porter <tylerpporter@gmail.com>
2020-05-18 20:15:45 -0600

    Refactor by_name scope

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,10 +4,7 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
 
-  scope :by_name, lambda { |name|
-    where('lower(name) LIKE ?', "%#{name}%")
-      .or(where('upper(name) LIKE ?', "%#{name}%"))
-  }
+  scope :by_name, ->(name) { where('lower(name) LIKE ?', "%#{name.downcase}%")}
   scope :by_created_at, ->(created_at) { where created_at: created_at }
   scope :by_updated_at, ->(updated_at) { where updated_at: updated_at }
 end
