6478de8ba48ffa1adec76885c7efaf7169bd75ff
Tyler Porter <tylerpporter@gmail.com>
2020-05-18 18:09:00 -0600

    Create find routes for items and merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      
+      namespace :items do
+        get '/find', to: 'search#show'
+      end
+
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
+
       resources :items, except: [:new, :edit]
       resources :merchants, except: [:new, :edit]
 
