988a3d2d9ef79801d8979e1d14205fc9d8dfa644
Tyler Porter <tylerpporter@gmail.com>
2020-05-18 19:33:06 -0600

    Create scopes for filtering resource

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,13 @@
 class Merchant < ApplicationRecord
-  has_many :items, dependent: :destroy 
-  has_many :invoices, dependent: :destroy 
+  include Filterable
+
+  has_many :items, dependent: :destroy
+  has_many :invoices, dependent: :destroy
+
+  scope :by_name, lambda { |name|
+    where('lower(name) LIKE ?', "%#{name}%")
+      .or(where('upper(name) LIKE ?', "%#{name}%"))
+  }
+  scope :by_created_at, ->(created_at) { where created_at: created_at }
+  scope :by_updated_at, ->(updated_at) { where updated_at: updated_at }
 end
